3cc2d62c9e87c730147e0c6645af4d9655350e40
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-10-30 18:50:43 +0300

    Complete the Application's User Interface (UI)

--- main.py
+++ main.py
@@ -17,16 +17,23 @@ window.title("Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
 
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+title_label.grid(column=1, row=0)
+
 canvas = Canvas(width= 200, height= 224, bg= YELLOW, highlightthickness=0) 
 tomato_img = PhotoImage(file= "pomodoro_timer/tomato.png") # to read through a file to get hold of that image.
 canvas.create_image(100, 112, image= tomato_img)
 canvas.create_text(100, 130, text= "00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+start_button = Button(text="Start", width=8, font=(FONT_NAME, 15, "bold"))
+start_button.grid(column=0, row=2)
 
+checkmark_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
+checkmark_label.grid(column=1, row=3)
 
-
-
+reset_button = Button(text="Reset", width=8, font=(FONT_NAME, 15, "bold"))
+reset_button.grid(column=2, row=2)
 
 
 
