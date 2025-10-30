e92df3c0f5360cdc30a8285172305827c388652f
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-10-30 18:00:04 +0300

    Initial commit(Creating the User Interface (UI))

---
+++ main.py
@@ -0,0 +1,53 @@
+from tkinter import *
+# ----CONSTANTS 
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+
+
+
+window = Tk()
+window.title("Timer")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+canvas = Canvas(width= 200, height= 224, bg= YELLOW, highlightthickness=0) 
+tomato_img = PhotoImage(file= "pomodoro_timer/tomato.png") # to read through a file to get hold of that image.
+canvas.create_image(100, 112, image= tomato_img)
+canvas.create_text(100, 130, text= "00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
